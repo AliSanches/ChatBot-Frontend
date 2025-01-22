9fa6582594ead4a7985cd55423cab1a53acc2bad
Ali.San <alissonsanches54@gmail.com>
2025-01-22 14:57:20 -0300

    Feat - Index - Frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import "./App.css";
+import { Provider } from "./components/ui/provider";
+import { BrowserRouter, Routes, Route } from "react-router";
+import { Index } from "./pages/Index/Index";
 
 function App() {
-  return <></>;
+  return (
+    <Provider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/index" element={<Index />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  );
 }
 
 export default App;
---
+++ src/pages/Index/Index.tsx
@@ -0,0 +1,62 @@
+import { Flex, Text, Button, HStack } from "@chakra-ui/react";
+import { IoNotifications } from "react-icons/io5";
+import { Avatar } from "../../components/ui/avatar";
+import {
+  MenuContent,
+  MenuItem,
+  MenuRoot,
+  MenuTrigger,
+} from "../../components/ui/menu";
+
+export const Index = () => {
+  return (
+    <header>
+      <HStack
+        h={"60px"}
+        bg={"#738ed3"}
+        alignItems={"center"}
+        justifyContent={"space-between"}
+      >
+        <div>
+          <Text px={4} fontWeight={"bold"} fontSize={"22px"}>
+            ChatBot
+          </Text>
+        </div>
+        <Flex pl={"auto"}>
+          <MenuRoot positioning={{ placement: "left-start" }}>
+            <MenuTrigger asChild>
+              <Button
+                background={"transparent"}
+                color={"white"}
+                outline={"transparent"}
+              >
+                <IoNotifications />
+              </Button>
+            </MenuTrigger>
+            <MenuContent>
+              <MenuItem value="new-txt">
+                Nenhuma notificação no momento...
+              </MenuItem>
+            </MenuContent>
+          </MenuRoot>
+
+          <MenuRoot>
+            <MenuTrigger asChild>
+              <Button background={"transparent"} outline={"transparent"}>
+                <Avatar
+                  name="Sasuke Uchiha"
+                  src="https://bit.ly/broken-link"
+                  colorPalette="teal"
+                />
+              </Button>
+            </MenuTrigger>
+            <MenuContent>
+              <MenuItem value="online">Online</MenuItem>
+              <MenuItem value="offline">Offline</MenuItem>
+            </MenuContent>
+          </MenuRoot>
+        </Flex>
+      </HStack>
+    </header>
+  );
+};
